50d1736dbed584e7d4fa789133dab2e034729b07
Vladimir Bavtenko <batonich84@mail.ru>
2022-05-30 09:16:12 +0300

    Удаляет лишние комменты.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,55 +6,6 @@ import {useState, useEffect} from 'react';
 import {Modal} from "./modal/modal";
 
 export const App: FC = () => {
-    /*const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        fetch('https://todo.doczilla.pro/api/todos')
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setItems(result);
-                    console.log(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
-
-    if (error) {
-        return <div>Ошибка</div>;
-    } else if (!isLoaded) {
-        return <div>Загрузка...</div>;
-    } else {
-        return (
-            <div className='wrapper'>
-                <Header/>
-                <Main/>
-            </div>
-            /!*<ul>
-                {items.map(item => (
-                    <li key={item.id}>
-                        {item.name} {item.price}
-                    </li>
-                ))}
-            </ul>*!/
-        );
-    }
-}*/
-// второй вариант через прокси-сервер
-   /* useEffect(() => {
-        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        const targetUrl = 'https://todo.doczilla.pro/api/todos';
-        fetch(proxyUrl + targetUrl)
-            .then(result => console.log(result))
-    }, []);*/
-
-
     const [modalActive, setModalActive] = useState(false);
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
